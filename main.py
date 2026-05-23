aa599574a1aff0f50ffc0972e6e22ff61b6a0745
Sajad Ahmadi Jozdani <61582647+sajadahmadia@users.noreply.github.com>
2026-05-23 21:49:02 +0200

    changed the homepage to login

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 # main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from routes import employee_routes, auth_routes
 from auth import auth_middleware
 
 app = FastAPI()
+
+
+@app.exception_handler(StarletteHTTPException)
+async def auth_redirect(request: Request, exc: StarletteHTTPException):
+    if exc.status_code in (401, 403):
+        return RedirectResponse('/login')
+    raise exc
 
 
 # Set up templates
